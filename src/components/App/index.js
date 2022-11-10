4a3658e1154c56836336508486963e4339f23a85
esperantkela <esperant.kela@gmail.com>
2022-11-10 18:11:00 +0100

    landing section

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,12 @@
 import '../../App.css';
 import Header from '../Header';
+import Landing from '../Landing';
 
 function App() {
   return (
     <div >
      <Header/>
+     <Landing/>
     </div>
   );
 }
