9a5f9f6cbbcdb0bd339101a765397ec873f2fdf9
esperantkela <esperant.kela@gmail.com>
2022-11-11 11:49:52 +0100

    Login component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,11 +2,13 @@ import '../../App.css';
 import Footer from '../Footer';
 import Header from '../Header';
 import Landing from '../Landing';
+import Welcome from '../Welcome';
 
 function App() {
   return (
     <div >
      <Header/>
+     <Welcome/>
      <Landing/>
      <Footer/>
     </div>
