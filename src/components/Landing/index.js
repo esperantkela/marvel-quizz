a0ab97f1b615cc3f87da4e702b797cf0ba1a025f
esperantkela <esperant.kela@gmail.com>
2022-11-11 11:01:56 +0100

    add and remove startingImg class when component dit mounted

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 
 const Landing = () => {
+
+  const [isBtn, setIsBtn] = useState(false);
+  const refWelverine = useRef(null)
+  useEffect(() => {
+    refWelverine.current.classList.add('startingImg')
+    setTimeout(()=>{
+      refWelverine.current.classList.remove('startingImg')
+      setIsBtn(true)
+    },1000)
+  }, []);
+
+  const displayBtn = isBtn && (
+    <React.Fragment>
+      <div className='leftBox'>
+            <button className='btn-welcome'>Inscription</button>
+        </div>
+
+        <div className='rightBox'>
+            <button className='btn-welcome'>Connexion</button>
+        </div>
+    </React.Fragment>
+  )
   return (
-    <main className='welcomePage'>
-        
+    <main ref={refWelverine} className='welcomePage'>
+        {displayBtn}
     </main>
   )
 }
