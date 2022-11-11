ac286bfbf1a084cd3059e8c167e717858ea39186
esperantkela <esperant.kela@gmail.com>
2022-11-11 15:47:07 +0100

    config firebase

---
+++ src/components/Firebase/firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const config = {
+    apiKey: "AIzaSyBHSzqot_hDwvQeXldxUhAN-genfGf7N4g",
+    authDomain: "marvel-quizz-1c4ab.firebaseapp.com",
+    projectId: "marvel-quizz-1c4ab",
+    storageBucket: "marvel-quizz-1c4ab.appspot.com",
+    messagingSenderId: "431332149388",
+    appId: "1:431332149388:web:3f25cb58261974cd1053a9"
+  };
+
+initializeApp(config);
