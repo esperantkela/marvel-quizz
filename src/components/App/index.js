830f959c9e7d2a085e16e4f669fd6d63f0676089
esperantkela <esperant.kela@gmail.com>
2022-11-11 13:13:01 +0100

    add react-router-dom

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,20 +1,23 @@
 import '../../App.css';
+import ErrorPage from '../ErrorPage';
 import Footer from '../Footer';
 import Header from '../Header';
 import Landing from '../Landing';
 import Login from '../Login';
 import SignUp from '../SignUp.js';
 import Welcome from '../Welcome';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div >
      <Header/>
 
-     <Welcome/>
-     <Landing/>
-     <Login/>
-     <SignUp/>
+     <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<App />} />
+        </Routes>
+     </BrowserRouter>
      <Footer/>
     </div>
   );
