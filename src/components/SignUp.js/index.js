ebb7c4733775f7fd70d95304a3228dc14199dbf7
esperantkela <esperant.kela@gmail.com>
2022-11-11 17:05:19 +0100

    create register form

--- src/components/SignUp.js/index.js
+++ src/components/SignUp.js/index.js
@@ -4,7 +4,32 @@ const SignUp = () => {
   return (
     <div className='signUpLoginBBox'>
        <div className='slContainer'>
-        SignUP
+            <div className='formBoxLeftSignup'>
+
+            </div>
+            <div className='formBoxRight'>
+                <div className='formContent'>
+                    <h2>Inscription</h2>
+                    <form>
+                        <div className='inputBox'>
+                            <input type='text' id='pseudo' required/>
+                            <label htmlFor='pseudo'>Pseudo</label>
+                        </div>
+                        <div className='inputBox'>
+                            <input type='email' id='email' required/>
+                            <label htmlFor='email'>Email</label>
+                        </div>
+                        <div className='inputBox'>
+                            <input type='password' id='password' required/>
+                            <label htmlFor='password'>Mot de passe</label>
+                        </div>
+                        <div className='inputBox'>
+                            <input type='confirmPassword' id='email' required/>
+                            <label htmlFor='confirmPasswordl'>Confirmer le mot de passe</label>
+                        </div>
+                    </form>
+                </div>
+            </div>
        </div>
     </div>
   )
