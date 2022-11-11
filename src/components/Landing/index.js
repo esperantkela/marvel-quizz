f8b892e3cca6f37b61e1bc3dca99579e7ee8c463
esperantkela <esperant.kela@gmail.com>
2022-11-11 11:34:03 +0100

    onMouseOut

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -19,13 +19,21 @@ const Landing = () => {
     refWelverine.current.classList.add('rightImg')
   }
 
+  const clearImg = () => {
+    if(refWelverine.current.classList.contains('rightImg')){
+      refWelverine.current.classList.remove('rightImg')
+    }else if (refWelverine.current.classList.contains('leftImg')){
+      refWelverine.current.classList.remove('leftImg')
+    }
+  }
+
   const displayBtn = isBtn && (
     <React.Fragment>
-      <div onMouseOver={setLeftImg} className='leftBox'>
+      <div onMouseOver={setLeftImg} onMouseOut={clearImg} className='leftBox'>
             <button className='btn-welcome'>Inscription</button>
         </div>
 
-        <div onMouseOver={setRightImg} className='rightBox'>
+        <div onMouseOver={setRightImg} onMouseOut={clearImg} className='rightBox'>
             <button className='btn-welcome'>Connexion</button>
         </div>
     </React.Fragment>
