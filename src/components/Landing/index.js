1f8b335dd9a62f161c9c008ba175f0202eb81b71
esperantkela <esperant.kela@gmail.com>
2022-11-11 11:18:28 +0100

    overMouseOver when we click on button

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -12,13 +12,20 @@ const Landing = () => {
     },1000)
   }, []);
 
+  const setLeftImg = () => {
+    refWelverine.current.classList.add('leftImg')
+  }
+  const setRightImg = () => {
+    refWelverine.current.classList.add('rightImg')
+  }
+
   const displayBtn = isBtn && (
     <React.Fragment>
-      <div className='leftBox'>
+      <div onMouseOver={setLeftImg} className='leftBox'>
             <button className='btn-welcome'>Inscription</button>
         </div>
 
-        <div className='rightBox'>
+        <div onMouseOver={setRightImg} className='rightBox'>
             <button className='btn-welcome'>Connexion</button>
         </div>
     </React.Fragment>
