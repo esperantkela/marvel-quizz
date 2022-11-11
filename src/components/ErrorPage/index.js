67bda39a79fb13c0c8c845d8ddd39bc6e1923f48
esperantkela <esperant.kela@gmail.com>
2022-11-11 14:40:04 +0100

    custom error page

--- src/components/ErrorPage/index.js
+++ src/components/ErrorPage/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import batman from '../../images/batman.png'
+
+const centerH2 = {
+     textAlign : 'center',
+     marginTop : '50px'
+}
+
+const centerImg = {
+    display : 'block',
+    marginTop : '40px auto'
+}
 
 const ErrorPage = () => {
   return (
     <div className='quiz-bg'>
         <div className='container'>
-           <h2>Oups!!! cette pas n'existe pas</h2>
+           <h2 style={centerH2}>Oups!!! cette pas n'existe pas</h2>
+           <img style={centerImg} src={batman} alt='batman'/>
         </div>
     </div>
   )
