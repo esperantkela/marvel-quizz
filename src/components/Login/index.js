5fccdefcc19e569142105efb29312636ca4dcca6
Espérant KELA <esperant.kela@gmail.com>
2022-11-24 17:29:00 +0100

    login page

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 
 const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [btn, setBtn] = useState('');
+
+    const handleEmail = (event) =>{
+        setEmail(event.target.value);
+    }
+    const handlePassword = (event) =>{
+        setPassword(event.target.value);
+    }
+
   return (
     <div className='signUpLoginBox'>
         <div className='slContainer'>
@@ -13,24 +25,16 @@ const Login = () => {
                     <h2>Connecxion</h2>
                     <form >
                         <div className='inputBox'>
-                            <input onChange={handleChange} value={pseudo} type='text' id='pseudo' required/>
-                            <label htmlFor='pseudo'>Pseudo</label>
-                        </div>
-                        <div className='inputBox'>
-                            <input onChange={handleChange} value={email} type='email' id='email' required/>
+                            <input onChange={handleEmail}       value={email} type='email' id='email' required/>
                             <label htmlFor='email'>Email</label>
                         </div>
                         <div className='inputBox'>
-                            <input onChange={handleChange} value={password} type='password' id='password' required/>
+                            <input onChange={handlePassword} value={password} type='password' id='password' required/>
                             <label htmlFor='password'>Mot de passe</label>
-                        </div>
-                        <div className='inputBox'>
-                            <input onChange={handleChange} value={confirmPassword} type='password' id='confirmPassword' required/>
-                            <label htmlFor='confirmPassword'>Confirmer le mot de passe</label>
                         </div>
                     </form>
                     <div className='linkContainer'>
-                        <Link className='simpleLink' to='/login'>Pas de compte? Inscrivez-vous</Link>
+                        <Link className='simpleLink' to='/register'>Pas de compte? Inscrivez-vous</Link>
                     </div>
                 </div>
             </div>
