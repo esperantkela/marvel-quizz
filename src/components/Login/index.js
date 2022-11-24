a7ba1691467cf2b5373ea7559027a284897ff6de
Espérant KELA <esperant.kela@gmail.com>
2022-11-24 18:19:00 +0100

    login with firebase

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,11 +1,17 @@
 import React, {useState, useEffect} from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
+import {signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../Firebase/firebaseConfig';
+
 
 const Login = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [btn, setBtn] = useState(false);
+    const [error, setError] = useState(false);
+
+    const navigate = useNavigate()
 
     const handleEmail = (event) =>{
         setEmail(event.target.value);
@@ -16,10 +22,28 @@ const Login = () => {
 
     useEffect(() => {
         
-        if(password.length > 5 && email != ''){
+        if(password.length > 5 && email !== ''){
             setBtn(true);
+        }else if(btn){
+            setBtn(false)
         }
-    }, [])
+    }, [email, password, btn])
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        
+        signInWithEmailAndPassword(auth, email, password)
+            .then((user) => {
+                setEmail('')
+                setPassword('')
+                navigate('/welcome', {replacee : true})
+            })
+            .catch((error) => {
+                setError(error)
+                setEmail('')
+                setPassword('')
+            });
+        }
 
   return (
     <div className='signUpLoginBox'>
@@ -29,8 +53,9 @@ const Login = () => {
             </div>
             <div className='formBoxRight'>
                 <div className='formContent'>
+                {error !== '' && <span>{error.message}</span>}
                     <h2>Connecxion</h2>
-                    <form >
+                    <form onSubmit={handleSubmit} >
                         <div className='inputBox'>
                             <input onChange={handleEmail}       value={email} type='email' id='email' required/>
                             <label htmlFor='email'>Email</label>
@@ -39,6 +64,8 @@ const Login = () => {
                             <input onChange={handlePassword} value={password} type='password' id='password' required/>
                             <label htmlFor='password'>Mot de passe</label>
                         </div>
+
+                        {<button disabled={btn ? false : true}>Connexion</button> }
                     </form>
                     <div className='linkContainer'>
                         <Link className='simpleLink' to='/register'>Pas de compte? Inscrivez-vous</Link>
