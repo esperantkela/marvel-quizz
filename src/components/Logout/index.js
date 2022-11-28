b42f4302f24360bc90a06226ac1727acf8cd62dc
esperantkela <esperant.kela@gmail.com>
2022-11-28 16:56:38 +0100

    firebase logout user

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { signOut } from 'firebase/auth';
+import { auth } from '../Firebase/firebaseConfig';
 
 const Logout = () => {
+
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = (e) =>{
+    setChecked(e.target.checked)
+  }
+
+  useEffect(() => {
+    if(checked){
+      signOut(auth).then(() => {
+        console.log('Déconnexion')
+      }).catch((error) => {
+        // An error happened.
+      });
+    }
+  }, [checked])
+  
   return (
     <div className='logoutContainer'>
         <label className='switch'>
             <input
+            onChange={handleChange}
               type='checkbox'
+              checked={checked}
             />
 
             <span className='slider round'></span>
