781329bb84f03baeba2f8b34252ba23287dfd22f
esperantkela <esperant.kela@gmail.com>
2022-11-28 17:44:28 +0100

    working progress

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Logout from '../Logout'
 import Quiz from '../Quiz'
 
 const Welcome = () => {
-  return (
-    <div children="quiz-bg">
+
+  const [userSession, setUserSession] = useState(null);
+
+  return userSession === null ? (
+    <React.Fragment>
+      <div className='loader'></div>
+      <p className='loaderText'>Loading...</p>
+    </React.Fragment>
+  ) :
+      <div children="quiz-bg">
         <div className="container">
           <Logout/>  
           <Quiz/>  
         </div>
     </div>
-  )
+  
 }
 
 export default Welcome
