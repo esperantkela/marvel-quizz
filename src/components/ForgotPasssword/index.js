bb53e48e7d002aa71ef94abd264b94c237feec06
esperantkela <esperant.kela@gmail.com>
2022-11-30 17:49:25 +0100

    working progress

--- src/components/ForgotPasssword/index.js
+++ src/components/ForgotPasssword/index.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 const ForgotPassword = () => {
+
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = () => {
+
+    }
+    
   return (
-    <div>ForgotPassword</div>
+    <div className='signUpLoginBox'>
+        <div className='slContainer'>
+            <div className='formBoxLeftForget'>
+
+            </div>
+            <div className='formBoxRight'>
+                <div className='formContent'>
+                    <h2>Connexion</h2>
+                    <form onSubmit={handleSubmit} >
+                        <div className='inputBox'>
+                            <input onChange={(e) => setEmail(e.target.value)}  value={email} type='email' id='email' required/>
+                            <label htmlFor='email'>Email</label>
+                        </div>
+                    </form>
+                    <div className='linkContainer'>
+                        <Link className='simpleLink' to='/register'>Pas de compte? Inscrivez-vous</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
