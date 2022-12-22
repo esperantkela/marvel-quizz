c398a5733f895476383d40655fd7609bc736197c
Espérant KELA <esperant.kela@gmail.com>
2022-12-22 07:37:40 +0100

    working progress

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -8,6 +8,12 @@ const Quiz = ({ userData }) => {
     <div>
       <Levels/>
       <ProgressBar/>
+      <h2>Notre question quiz</h2>
+      <p className='answerOptions'>Question 1</p>
+      <p className='answerOptions'>Question 2</p>
+      <p className='answerOptions'>Question 3</p>
+      <p className='answerOptions'>Question 4</p>
+      <button className='btnSubmit'>Suivant</button>
     </div>
   )
 }
