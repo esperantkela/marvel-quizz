3bb458a794a22d975388fd6f3e22d51fe7166765
Espérant KELA <esperant.kela@gmail.com>
2022-12-22 07:30:24 +0100

    working progress

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Levels from '../Levels'
+import ProgressBar from '../ProgressBar'
 
 const Quiz = ({ userData }) => {
   
   return (
     <div>
       <Levels/>
+      <ProgressBar/>
     </div>
   )
 }
