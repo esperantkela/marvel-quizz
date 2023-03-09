202ab56ea252a537da6840754e9e92f091ab945a
esperantkela <esperant.kela@gmail.com>
2023-03-09 16:10:14 +0100

    working progress

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -10,16 +10,39 @@ class Quiz extends Component{
     super()
     this.state = {
       'levelNames' : ['debutant','confirme','Expert'],
-      'quizLevel' : 0
+      'quizLevel' : 0,
+      'maxQuestions' : 10,
+      'storedQuestions' : [],
+      'question' : null,
+      'options' : [],
+      'idQuestion' : 0
     }
   }
 
   loadQuestions = (quizz) => {
     const fetchArrayQuiz =  QuizMarvel[0].quizz[quizz]
+    if(fetchArrayQuiz >= this.maxQuestions){
+      const newArray = fetchArrayQuiz.map(({answer, keepRest}) => keepRest)
+
+      this.setState({
+        storedQuestions: newArray
+      })
+    }else{
+      console.log('pas assez de questions !!')
+    }
   }
 
   componentDidMount(){
     this.loadQuestions(this.state.levelNames[this.state.quizLevel])
+  }
+
+  componentDidUpdate(prevState, prevProps) {
+    if(this.state.storedQuestions !== prevState.storedQuestions){
+      this.setState({
+        'question' : this.state.storedQuestions[0].question,
+        'options' : this.state.storedQuestions[0].options
+      })
+    }
   }
   render(){
     
