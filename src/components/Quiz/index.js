f60e0fc220575156046389ea3e5f980a3e3b5cab
esperantkela <esperant.kela@gmail.com>
2023-03-09 15:15:16 +0100

    working progress

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,42 +1,21 @@
-import React, {useState,useEffect} from 'react'
+import React, {useState,useEffect, Component} from 'react'
 import Levels from '../Levels'
 import ProgressBar from '../ProgressBar'
 import { QuizMarvel } from '../QuizMarvel'
 
-const Quiz = ({ userData }) => {
-  
-  const [levelNames, setLevelNames] = useState(['debutant', 'confirme', 'expert']);
-  const [quizLevel, setQuizLevel] = useState(0);
-  const [maxQuestions, setMaxQuestions] = useState(10);
-  const [storedQuestions, setStoredQuestions] = useState([]);
 
-  const loadQuestions = (quiz) => {
-    const fetchArrayQuiz = QuizMarvel[0].quizz[quiz]
-      if(fetchArrayQuiz.length >= maxQuestions) {
-          const newArray = fetchArrayQuiz.map(({answer, ...keepRest}) => keepRest)
-          setStoredQuestions(newArray)
-          console.log(storedQuestions)
-      }else{
-        console.log('pas assez de questions')
-      }
+
+class Quiz extends Component{
+  render(){
+    return (
+      <div>
+        <Levels/>
+        <ProgressBar/>
+        <h2>Notre question Quiz</h2>
+        <p className='answerOptions'>Question 1</p>
+      </div>
+    )
   }
-
-  useEffect(() => {
-    loadQuestions(levelNames[quizLevel])
-  }, []);
-  
-  return (
-    <div>
-      <Levels/>
-      <ProgressBar/>
-      <h2>Notre question quiz</h2>
-      <p className='answerOptions'>Question 1</p>
-      <p className='answerOptions'>Question 2</p>
-      <p className='answerOptions'>Question 3</p>
-      <p className='answerOptions'>Question 4</p>
-      <button className='btnSubmit'>Suivant</button>
-    </div>
-  )
 }
 
 export default Quiz
