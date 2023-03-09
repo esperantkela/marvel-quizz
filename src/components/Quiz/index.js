469c227d9d15af21a45c277044bf8cce08d25a73
esperantkela <esperant.kela@gmail.com>
2023-03-09 16:36:31 +0100

    fixed bug

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -6,24 +6,21 @@ import { QuizMarvel } from '../QuizMarvel'
 
 
 class Quiz extends Component{
-  constructor(props){
-    super()
-    this.state = {
-      'levelNames' : ['debutant','confirme','Expert'],
-      'quizLevel' : 0,
-      'maxQuestions' : 10,
-      'storedQuestions' : [],
-      'question' : null,
-      'options' : [],
-      'idQuestion' : 0
-    }
+  state = {
+    'levelNames' : ['debutant','confirme','Expert'],
+    'quizLevel' : 0,
+    'maxQuestions' : 10,
+    'storedQuestions' : [],
+    'question' : null,
+    'options' : [],
+    'idQuestion' : 0
   }
-
+  
   loadQuestions = (quizz) => {
     const fetchArrayQuiz =  QuizMarvel[0].quizz[quizz]
-    if(fetchArrayQuiz >= this.maxQuestions){
-      const newArray = fetchArrayQuiz.map(({answer, keepRest}) => keepRest)
-
+    if(fetchArrayQuiz.length >= this.state.maxQuestions){
+      const newArray = fetchArrayQuiz.map(({answer, ...keepRest}) => keepRest)
+      console.log(newArray)
       this.setState({
         storedQuestions: newArray
       })
@@ -36,11 +33,11 @@ class Quiz extends Component{
     this.loadQuestions(this.state.levelNames[this.state.quizLevel])
   }
 
-  componentDidUpdate(prevState, prevProps) {
+  componentDidUpdate(prevProps, prevState) {
     if(this.state.storedQuestions !== prevState.storedQuestions){
       this.setState({
-        'question' : this.state.storedQuestions[0].question,
-        'options' : this.state.storedQuestions[0].options
+        'question': this.state.storedQuestions[this.state.idQuestion].question,
+        'options': this.state.storedQuestions[this.state.idQuestion].options
       })
     }
   }
@@ -50,7 +47,7 @@ class Quiz extends Component{
       <div>
         <Levels/>
         <ProgressBar/>
-        <h2>Notre question Quiz</h2>
+        <h2>{this.state.question}</h2>
         <p className='answerOptions'>Question 1</p>
         <p className='answerOptions'>Question 2</p>
         <p className='answerOptions'>Question 3</p>
