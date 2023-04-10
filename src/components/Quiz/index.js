d964da38d7b2d61fab28f8c6da0742ceb9bdcbe9
esperantkela <esperant.kela@gmail.com>
2023-04-10 17:01:43 +0100

    format code

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,77 +1,81 @@
-import React, {useState,useEffect, Component} from 'react'
-import Levels from '../Levels'
-import ProgressBar from '../ProgressBar'
-import { QuizMarvel } from '../QuizMarvel'
+import React, { useState, useEffect, Component } from "react";
+import Levels from "../Levels";
+import ProgressBar from "../ProgressBar";
+import { QuizMarvel } from "../QuizMarvel";
 
-
-
-class Quiz extends Component{
+class Quiz extends Component {
   state = {
-    'levelNames' : ['debutant','confirme','Expert'],
-    'quizLevel' : 0,
-    'maxQuestions' : 10,
-    'storedQuestions' : [],
-    'question' : null,
-    'options' : [],
-    'idQuestion' : 0,
-    'btnDisabled' : true,
-    'userAnswer' : null,
-  }
-  
-  loadQuestions = (quizz) => {
-    const fetchArrayQuiz =  QuizMarvel[0].quizz[quizz]
-    if(fetchArrayQuiz.length >= this.state.maxQuestions){
-      const newArray = fetchArrayQuiz.map(({answer, ...keepRest}) => keepRest)
-      console.log(newArray)
-      this.setState({
-        storedQuestions: newArray
-      })
-    }else{
-      console.log('pas assez de questions !!')
-    }
-  }
+    levelNames: ["debutant", "confirme", "Expert"],
+    quizLevel: 0,
+    maxQuestions: 10,
+    storedQuestions: [],
+    question: null,
+    options: [],
+    idQuestion: 0,
+    btnDisabled: true,
+    userAnswer: null,
+  };
 
-  componentDidMount(){
-    this.loadQuestions(this.state.levelNames[this.state.quizLevel])
+  loadQuestions = (quizz) => {
+    const fetchArrayQuiz = QuizMarvel[0].quizz[quizz];
+    if (fetchArrayQuiz.length >= this.state.maxQuestions) {
+      const newArray = fetchArrayQuiz.map(
+        ({ answer, ...keepRest }) => keepRest
+      );
+      console.log(newArray);
+      this.setState({
+        storedQuestions: newArray,
+      });
+    } else {
+      console.log("pas assez de questions !!");
+    }
+  };
+
+  componentDidMount() {
+    this.loadQuestions(this.state.levelNames[this.state.quizLevel]);
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if(this.state.storedQuestions !== prevState.storedQuestions){
+    if (this.state.storedQuestions !== prevState.storedQuestions) {
       this.setState({
-        'question': this.state.storedQuestions[this.state.idQuestion].question,
-        'options': this.state.storedQuestions[this.state.idQuestion].options
-      })
+        question: this.state.storedQuestions[this.state.idQuestion].question,
+        options: this.state.storedQuestions[this.state.idQuestion].options,
+      });
     }
   }
 
-  submitAnswer = (selectedAnswer) =>{
+  submitAnswer = (selectedAnswer) => {
     this.setState({
-      'userAnswer': selectedAnswer,
-      'btnDisabled' : false
-    })
-
-  }
-  render(){
+      userAnswer: selectedAnswer,
+      btnDisabled: false,
+    });
+  };
+  render() {
     const displayOptions = this.state.options.map((option, index) => {
       return (
-        <p key={index} 
-          className={`answerOptions ${this.state.userAnswer === option ? 'selected' : ''}`}
+        <p
+          key={index}
+          className={`answerOptions ${
+            this.state.userAnswer === option ? "selected" : ""
+          }`}
           onClick={() => this.submitAnswer(option)}
         >
           {option}
         </p>
-      )
-    })
+      );
+    });
     return (
       <div>
-        <Levels/>
-        <ProgressBar/>
+        <Levels />
+        <ProgressBar />
         <h2>{this.state.question}</h2>
         {displayOptions}
-        <button disabled={this.state.btnDisabled} className='btnSubmit'>Suivant</button>
+        <button disabled={this.state.btnDisabled} className="btnSubmit">
+          Suivant
+        </button>
       </div>
-    )
+    );
   }
 }
 
-export default Quiz
+export default Quiz;
